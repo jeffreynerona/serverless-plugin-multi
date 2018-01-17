7e624a1a724142c3f83a5c1a30805f2ad63fdd82
Benjamin Forster <benjamin.forster@loanmarket.com.au>
2018-01-18 10:51:41 +1100

    add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -28,6 +28,8 @@ describe('Serverless Multi', () => {
     };
     serverless = new Serverless(options);
     serverless.cli = new serverless.classes.CLI(serverless);
+    serverless.pluginManager = new serverless.classes.PluginManager(serverless);
+    // get an array of commands and options that should be processed
     serverless.service.service = 'myService';
     serverless.config.servicePath = '';
     serverless.service.package = {};
@@ -39,7 +41,10 @@ describe('Serverless Multi', () => {
   });
 
   describe('constructor', () => {
-    it('returns Multi object with hook `before:package:initialize`', () => {
+    it('returns Multi object with hooks that extend all other plugins commands', () => {
+      serverless.pluginManager.loadCorePlugins();
+      const commands = serverless.pluginManager.getCommands();
+      // console.log(commands);
       plugin = new Multi(serverless);
       const expectedHooks = [
         'multi:add:run',
@@ -47,10 +52,81 @@ describe('Serverless Multi', () => {
         'multi:generate:run',
         'multi:generate:symlinks',
       ];
-      expect(plugin.hooks).to.have.all.keys(expectedHooks);
+      Object.keys(commands).forEach((name) => {
+        expectedHooks.push(`multi:${name}:symlinks`);
+        expectedHooks.push(`multi:${name}:run`);
+      });
+      expect(plugin.hooks).to.have.includes.keys(expectedHooks);
+    });
+
+    xit('returns Multi object with multi style commands for all other plugins', () => {
+      serverless.pluginManager.loadCorePlugins();
     });
   });
 
-  describe('hook before:package:initialize', () => {
+  describe('generate command', () => {
+    context('without options', () => {
+      it('generates serverless config and symlinks for all services');
+    });
+
+    context('with service option', () => {
+      it('generates serverless config and symlinks for specified service');
+    });
+
+    context('with service option as comma seporated list', () => {
+      it('generates serverless config and symlinks for specified services');
+    });
+  });
+
+  describe('run [name] command', () => {
+    const commands = ['deploy', 'package', 'print'];
+
+    describe('hook execution order', () => {
+      let hooks;
+      beforeEach(() => {
+        serverless.pluginManager.loadCorePlugins();
+        serverless.pluginManager.addPlugin(Multi);
+        hooks = serverless.pluginManager.hooks;
+      });
+
+      commands.forEach((command) => {
+        it(`calls hooks "multi:${command}:symlinks" then "multi:${command}:deploy"`, () => {
+          const symlinkHook = hooks[`multi:${command}:symlinks`].find(x => x.pluginName === 'DeployMultiplePlugin');
+          const runHook = hooks[`multi:${command}:run`].find(x => x.pluginName === 'DeployMultiplePlugin');
+          const symlinksFn = sandbox.stub(symlinkHook, 'hook');
+          const runFn = sandbox.stub(runHook, 'hook');
+          return serverless.pluginManager.run(['multi', command])
+            .then(() => {
+              expect(symlinksFn.calledOnce).to.equal(true);
+              expect(runFn.calledOnce).to.equal(true);
+              expect(runFn.calledAfter(symlinksFn)).to.equal(true);
+            });
+        });
+      });
+    });
+
+    context('without options', () => {
+      it('runs specified command on for all services');
+    });
+
+    context('with service option', () => {
+      it('generates serverless config and symlinks for specified service');
+      it('runs specified command on for specified service');
+    });
+
+    context('with service option as comma seporated list', () => {
+      it('generates serverless config and symlinks for specified services');
+      it('runs specified command on for specified services');
+    });
+  });
+
+  describe('run offline command', () => {
+    it('runs spawns `offline:start hook`');
+  });
+
+  describe('add command (not implemented yet)', () => {
+    it('adds a new micro service template');
+    it('if no service directory set, uses default');
+    it('throws error if no name specified');
   });
 });
